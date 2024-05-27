4c587e0b7a1004757555782b8e319a56da1f3953
Katherine Kelly <ktkelly11@gmail.com>
2024-05-27 12:53:03 -0400

    added the form requirement

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express();
 const PORT = 3000;
 const fs = require("fs");
 
+// added form for the requirement
+app.get("/users/new", (req, res) => {
+  res.sendFile("/views/newUser.html", {
+    root: __dirname,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
