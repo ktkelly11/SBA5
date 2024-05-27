49492341edf000b426bb9721e64df2a17ada1a71
Katherine Kelly <ktkelly11@gmail.com>
2024-05-27 12:20:12 -0400

    added locations data

---
+++ data/locations.js
@@ -0,0 +1,62 @@
+const locations = [
+  {
+    post_id: 1,
+    location: "Leganes",
+    country: "Spain",
+    date: "8/10/2023",
+  },
+  {
+    post_id: 2,
+    location: "Fullerton",
+    country: "United States",
+    date: "10/19/2023",
+  },
+  {
+    post_id: 3,
+    location: "Kuantan",
+    country: "Malaysia",
+    date: "5/15/2024",
+  },
+  {
+    post_id: 4,
+    location: "Ertai",
+    country: "China",
+    date: "9/12/2023",
+  },
+  {
+    post_id: 5,
+    location: "Tsuruga",
+    country: "Japan",
+    date: "7/22/2023",
+  },
+  {
+    post_id: 6,
+    location: "Zhangdian",
+    country: "China",
+    date: "3/1/2024",
+  },
+  {
+    post_id: 7,
+    location: "Tiznit",
+    country: "Morocco",
+    date: "5/7/2024",
+  },
+  {
+    post_id: 8,
+    location: "Chambéry",
+    country: "France",
+    date: "10/12/2023",
+  },
+  {
+    post_id: 9,
+    location: "Moerewa",
+    country: "New Zealand",
+    date: "11/11/2023",
+  },
+  {
+    post_id: 10,
+    location: "Tegarenkrajan",
+    country: "Indonesia",
+    date: "2/7/2024",
+  },
+];
