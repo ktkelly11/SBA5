64a299ed807e31d0e115163b170821d5975ed0b6
Katherine Kelly <ktkelly11@gmail.com>
2024-05-26 22:05:37 -0400

    added utilities and copied error handling middleware

---
+++ utilities/error.js
@@ -0,0 +1,9 @@
+// Example of error handling middleware
+
+// function error(status, msg) {
+//   const err = new Error(msg);
+//   err.status = status;
+//   return err;
+// }
+
+// module.exports = error;
