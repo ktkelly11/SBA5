2485a195ef9feb145c5fae879fd3bdeb0c05f503
Katherine Kelly <ktkelly11@gmail.com>
2024-05-27 21:34:25 -0400

    Adding GET request to users route

--- routes/users.js
+++ routes/users.js
@@ -2,4 +2,38 @@ const express = require("express");
 const router = express.Router();
 const users = require("../data/users");
 
+// GET route for entire users database
+router.get("/", (req, res) => {
+  const links = [
+    {
+      href: "users/:id",
+      rel: ":id",
+      type: "GET",
+    },
+  ];
+
+  res.json({ users, links });
+});
+
+// GET route for individual users
+router.get("/:id", (req, res, next) => {
+  const user = users.find((u) => u.id == req.params.id);
+
+  const links = [
+    {
+      href: `/${req.params.id}`,
+      rel: "",
+      type: "PATCH",
+    },
+    {
+      href: `/${req.params.id}`,
+      rel: "",
+      type: "DELETE",
+    },
+  ];
+
+  if (user) res.json({ user, links });
+  else next();
+});
+
 module.exports = router;
